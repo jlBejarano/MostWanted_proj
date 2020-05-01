d5776e212dca51561d762c72316436c9694840c2
jorge bejarano <jorge.bejarano@yahoo.com>
2020-05-01 12:01:38 -0400

    edited display persons

--- app.js
+++ app.js
@@ -189,16 +189,16 @@ function displayPerson(person){
   // height, weight, age, name, occupation, eye color.
   let personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
-  let personInfo = "Height" + "/n";
-  personInfo += "Height" + person.height + "/n";
-  let personInfo = "Weight" + person.weight + "/n";
-  personInfo += "Weight" + person.weight + "/n";
-  let personInfo = "Age" + "/n";
-  personInfo += "Age" + person.age + "/n";
-  let personInfo = "Occupation"+ "/n";
-  personInfo += "Occupation"+ person.occupation + "/n";
-  let personInfo ="Eye Color" + "/n";
-  personInfo += "Eye Color" + person.eyecolor + "/n";
+  let personInfo = "Height" + "\n";
+  personInfo += "Height" + person.height + "\n";
+  let personInfo = "Weight" + person.weight + "\n";
+  personInfo += "Weight" + person.weight + "\n";
+  let personInfo = "Age" + "\n";
+  personInfo += "Age" + person.age + "\n";
+  let personInfo = "Occupation"+ "\n";
+  personInfo += "Occupation"+ person.occupation + "\n";
+  let personInfo ="Eye Color" + "\n";
+  personInfo += "Eye Color" + person.eyecolor + "\n";
 }
 // TODO: finish getting the rest of the information to display
 alert(personInfo);
